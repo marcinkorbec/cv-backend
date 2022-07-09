0b57fd5f0451a33fd28e07359d368da0e5f00eb2
marcinkorbec <62883259+marcinkorbec@users.noreply.github.com>
2022-07-09 17:50:25 +0200

    Refactor file

--- index.ts
+++ index.ts
@@ -2,21 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import 'express-async-errors'
 import {handleError} from "./utils/error";
+import {appRouter} from "./routes/appRouter";
 
 const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
 app.use(express.json());
-
-//Routes...
-
-app.get('/', async (req, res) => {
-    throw new Error('Oj no!');
-})
+app.use('/', appRouter)
 
 app.use(handleError);
 
-app.listen(3001, '0.0.0.0', () => {
-    console.log('Aplikacja nakurwia na porcie http://localhost:3001')
+app.listen(3021, '0.0.0.0', () => {
+    console.log('Aplikacja nakurwia na porcie http://localhost:3021')
 })
